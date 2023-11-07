1b1adbcc75708a8726086425a5e8a311a46b84e4
Hashim Alkhatab <Legendslayztoday@gmail.com>
2023-11-07 11:13:31 -0800

    Finsihed the problems

---
+++ Problem 1.py
@@ -0,0 +1,14 @@
+#Hashim Alkhatab
+#Date: November 6th 2023
+#Problem: 1
+name=input("what is your name ")
+contnum=-1
+print(f"hello {name} its nice to meet you")
+totalnum=0
+continents=["asia","europe","north america","south america","australia", "africa","antartica"]
+for continent in continents:
+    contnum+=1
+    contQ=input(f"have you been to {continents[contnum]} ")
+    if contQ.lower()=="yes":
+        totalnum+=1
+print(f"you have visited {totalnum}/7 continents")
---
+++ Problem 2.py
@@ -0,0 +1,19 @@
+#Hashim Alkhatab
+#Date: November 6th 2023
+#Problem: 2
+#the score average = 0
+score_average = 0
+#asks how many courses the user is taking
+courses = int(input("How many courses are you taking this semester?: ")) 
+#for the amount of courses they have they will print that amount and ask you the rate it.
+for courses in range(0,courses): 
+    rating_courses = int(input(f"What would you course out of 5: "))
+    score_average += rating_courses
+#this determines the average rating of the code and gives you a decent,average or bad feeback
+courses = courses + 1
+if score_average/courses <= 1:
+    print(f"{score_average/courses} OUCH!")
+elif score_average/courses <=3>1:
+    print(f"{score_average/courses} Not a bad semester!")
+else:
+    print(f"{score_average/courses} Glad to hear it!")
